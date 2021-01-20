b68e99f2f9ee54e59ca6954bd781d18dc0c7a5e6
taku matsunaga <skyline199306@yahoo.co.jp>
2021-01-20 12:22:08 +0900

    fix main design

--- resources/views/contents/main.blade.php
+++ resources/views/contents/main.blade.php
@@ -18,16 +18,16 @@
         <div>ここに画像を表示</div>
         <div style="display: flex; background-color: lightblue">
             <div>
-                <a href="{{ route('selectPants') }}">Pantsを選ぶ</a>
-                <p>カラー:{{$getPantsSet->color}}</p>
-                <p>ブランド:{{$getPantsSet->brand}}</p>
-                <p>対象:{{$getPantsSet->gender}}</p>
-            </div>
-            <div>
                 <a href="{{ route('selectTops') }}">Topsを選ぶ</a>
                 <p>カラー:{{$getTopsSet->color}}</p>
                 <p>ブランド:{{$getTopsSet->brand}}</p>
                 <p>対象:{{$getTopsSet->gender}}</p>
+            </div>
+            <div>
+                <a href="{{ route('selectPants') }}">Pantsを選ぶ</a>
+                <p>カラー:{{$getPantsSet->color}}</p>
+                <p>ブランド:{{$getPantsSet->brand}}</p>
+                <p>対象:{{$getPantsSet->gender}}</p>
             </div>
             <div>
                 <a href="{{ route('selectShoes') }}">Shoesを選ぶ</a>
@@ -37,21 +37,22 @@
             </div>
         </div>
     </div>
-    <div>
-        @if (isset($userInfo))
-        <p>ユーザーのお気に入りPants</p>
-        <p>{{$userInfo->favPants}}</p>
-        @if (isset($getPantsImg))
-        <img src="{{ asset('img/img_pants/' . $getPantsImg->img) }}" alt="{{$getPantsImg->img}}">
-        @endif
-        @endif
-    </div>
+
     <div>
         @if (isset($userInfo))
         <p>ユーザーのお気に入りTops</p>
         <p>{{$userInfo->favTops}}</p>
         @if (isset($getTopsImg))
         <img src="{{ asset('img/img_tops/' . $getTopsImg->img) }}" alt="{{$getTopsImg->img}}">
+        @endif
+        @endif
+    </div>
+    <div>
+        @if (isset($userInfo))
+        <p>ユーザーのお気に入りPants</p>
+        <p>{{$userInfo->favPants}}</p>
+        @if (isset($getPantsImg))
+        <img src="{{ asset('img/img_pants/' . $getPantsImg->img) }}" alt="{{$getPantsImg->img}}">
         @endif
         @endif
     </div>
