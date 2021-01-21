ee1e00dc98efbcc17062036418416a9318fb9296
taku matsunaga <skyline199306@yahoo.co.jp>
2021-01-21 19:50:07 +0900

    create migration file

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,6 +21,9 @@ class CreateUsersTable extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->integer('favPants');
+            $table->integer('favTops');
+            $table->integer('favShoes');
         });
     }
 
