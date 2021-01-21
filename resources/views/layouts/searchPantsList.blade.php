f389cee0b8bb3b88c30724b955349f029a0ac228
taku matsunaga <skyline199306@yahoo.co.jp>
2021-01-22 01:34:48 +0900

    fix search list and mannequin layout

--- resources/views/layouts/searchPantsList.blade.php
+++ resources/views/layouts/searchPantsList.blade.php
@@ -7,26 +7,30 @@
 
     {{-- {{$topsItemsOutputs}} --}}
 
-    <p>Pantsの条件に一致したアイテム</p>
+    <div class="searchListText">
+        <p>Pantsの条件に一致したアイテム</p>
+    </div>
     @if ($pantsItemsOutputs->isEmpty())
+    <div class="searchListText">
         <p>条件にあったアイテムはありませんでした。</p>
+    </div>
     @endif
     <div class="searchItemList_container">
         @foreach ($pantsItemsOutputs as $pantsItemsOutput)
-        <div>
+        <div class="searchItemList_box">
             <form action="{{ route('sendPants') }}" method="post">
                 @csrf
                 <li>
-                   <p>{{$pantsItemsOutput->id}}</p>
+                   {{-- <p>{{$pantsItemsOutput->id}}</p> --}}
                    <input type="hidden" name="pantsItemsOutputId" value="{{$pantsItemsOutput->id}}">
                    <p>{{$pantsItemsOutput->jancode}}</p>
                    <p>{{$pantsItemsOutput->adult}}</p>
-                   <p>{{$pantsItemsOutput->price}}</p>
+                   <p>¥ {{number_format($pantsItemsOutput->price)}}</p>
                    <p>{{$pantsItemsOutput->brand}}</p>
                    <p>{{$pantsItemsOutput->category}}</p>
                    <p>{{$pantsItemsOutput->color}}</p>
                 </li>
-                <input type="submit" value="選ぶ">
+                <input type="submit" value="選択">
             </form>
         </div>
             @endforeach
--- resources/views/layouts/searchTopsList.blade.php
+++ resources/views/layouts/searchTopsList.blade.php
@@ -5,26 +5,30 @@
 
     {{-- {{$topsItemsOutputs}} --}}
 
-    <p>Topsの条件に一致したアイテム</p>
+    <div class="searchListText">
+        <p>Topsの条件に一致したアイテム</p>
+    </div>
     @if ($topsItemsOutputs->isEmpty())
+    <div class="searchListText">
         <p>条件にあったアイテムはありませんでした。</p>
+    </div>
     @endif
     <div class="searchItemList_container">
         @foreach ($topsItemsOutputs as $topsItemsOutput)
-        <div>
+        <div class="searchItemList_box">
             <form action="{{ route('sendTops') }}" method="post">
                 @csrf
                 <li>
-                   <p>{{$topsItemsOutput->id}}</p>
+                   {{-- <p>{{$topsItemsOutput->id}}</p> --}}
                    <input type="hidden" name="topsItemsOutputId" value="{{$topsItemsOutput->id}}">
                    <p>{{$topsItemsOutput->jancode}}</p>
                    <p>{{$topsItemsOutput->adult}}</p>
-                   <p>{{$topsItemsOutput->price}}</p>
+                   <p>¥ {{number_format($topsItemsOutput->price)}}</p>
                    <p>{{$topsItemsOutput->brand}}</p>
                    <p>{{$topsItemsOutput->category}}</p>
                    <p>{{$topsItemsOutput->color}}</p>
                 </li>
-                <input type="submit" value="選ぶ">
+                <input type="submit" value="選択">
             </form>
         </div>
             @endforeach
