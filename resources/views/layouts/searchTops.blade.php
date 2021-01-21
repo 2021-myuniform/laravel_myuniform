460f4d70a82333876374994e6bda82f64f6fabdb
taku matsunaga <skyline199306@yahoo.co.jp>
2021-01-21 18:04:21 +0900

    fix search design

--- resources/views/layouts/searchTops.blade.php
+++ resources/views/layouts/searchTops.blade.php
@@ -25,7 +25,7 @@
 
         <input type="hidden" name="type" value="tops">
 
-        <div>
+        <div class="ask">
             <p>お探しのトップスカラーを教えてください。</p>
         </div>
 
@@ -54,30 +54,93 @@
             </ul>
         </div>
 
+        <div class="ask">
+            <p>お探しのブランドを教えてください。</p>
+        </div>
 
+        <div>
+            <ul class="brandSelect">
+                <li class="brandSelectList">
+                    <input class="brandSelectInput" type="radio" name="brand" value="yonex" id="yonexBrand">
+                    <label class="brandBox" for="yonexBrand">Yonex</label>
+                </li>
+                <li class="brandSelectList">
+                    <input class="brandSelectInput" type="radio" name="brand" value="nike" id="nikeBrand">
+                    <label class="brandBox" for="nikeBrand">NIKE</label>
+                </li>
+                <li class="brandSelectList">
+                    <input class="brandSelectInput" type="radio" name="brand" value="Adidas" id="adidasBrand">
+                    <label class="brandBox" for="adidasBrand">Adidas</label>
+                </li>
+            </ul>
+        </div>
 
-        <select name="gender" id="">
-            <option value="male">Male</option>
-            <option value="female">Female</option>
-        </select>
-        <select name="target" id="">
-            <option value="adult">Adult</option>
-        </select>
-        <select name="brand" id="">
-            <option value="yonex">Yonex</option>
-        </select>
-        <select name="category" id="">
-            <option value="long">Long</option>
-            <option value="half">Half</option>
-        </select>
-        {{-- <select name="color" id="">
-            <option value="black">黒</option>
-            <option value="yellow">黄</option>
-            <option value="blue">青</option>
-            <option value="red">赤</option>
-            <option value="pink">ピンク</option>
-        </select> --}}
-        <input type="submit" value="Submit">
+        <div class="ask">
+            <p>お探しの性別を選んでください。</p>
+        </div>
+
+        <div>
+            <ul class="genderSelect">
+                <li class="genderSelectList">
+                    <input class="genderSelectInput" type="radio" name="gender" value="male" id="maleGender">
+                    <div class="genderText">
+                        男性
+                    </div>
+                </li>
+                <li class="genderSelectList">
+                    <input class="genderSelectInput" type="radio" name="gender" value="female" id="femaleGender">
+                    <div class="genderText">
+                        女性
+                    </div>
+                </li>
+            </ul>
+        </div>
+
+        <div class="ask">
+            <p>お探しの対象を教えてください。</p>
+        </div>
+
+        <div>
+            <ul class="targetSelect">
+                <li class="targetSelectList">
+                    <input class="targetSelectInput" type="radio" name="target" value="adult" id="adultTarget">
+                    <div class="targetText">
+                        大人
+                    </div>
+                </li>
+                <li class="targetSelectList">
+                    <input class="targetSelectInput" type="radio" name="target" value="child" id="childTarget">
+                    <div class="targetText">
+                        子供
+                    </div>
+                </li>
+            </ul>
+        </div>
+
+        <div class="ask">
+            <p>お探しのウェアのタイプをお選びください。</p>
+        </div>
+
+        <div>
+            <ul class="categorySelect">
+                <li class="categorySelectList">
+                    <input class="categorySelectInput" type="radio" name="category" value="long" id="longCategory">
+                    <div class="categoryText">
+                        長袖
+                    </div>
+                </li>
+                <li class="categorySelectList">
+                    <input class="categorySelectInput" type="radio" name="category" value="half" id="halfCategory">
+                    <div class="categoryText">
+                        半袖
+                    </div>
+                </li>
+            </ul>
+        </div>
+
+        <div class="nextBtnBox">
+            <input class="nextBtn" type="submit" value="次に進む">
+        </div>
     </form>
 </div>
 
